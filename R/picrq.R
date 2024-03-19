b2adcf7597536266503d9efd80dcab73c584b3bb
Yeji Kim <116063129+yejikim1202@users.noreply.github.com>
2024-03-19 10:27:04 -0400

    Update picrq.R

--- R/picrq.R
+++ R/picrq.R
@@ -10,7 +10,7 @@ NULL
 #' @param x X matrix of baseline covariates.
 #' @param tau quantile level.
 #' @param estimation estimating method of partly interval censored, if estimation="DR", doubly robust estimator is estimated.
-#' @param application modified estimating method wiith different weighting position is applied, set \code{application=TRUE}.
+#' @param application modified estimating method is applied, set \code{application=TRUE}.
 #' @param var.estimation variance estimating method, if \code{var.estimation="IS"}, the induced smoothing method is used, and else if \code{var.estimation="Bootstrap"}, variance bootstrapping method is used.
 #' @param wttype weight estimating method, default is "KM", Beran's nonparametric KM estimating method as "Beran", and  Ishwaran's random survival forests KM estimating method as "Ishwaran".
 #' @param hlimit bandwidth value, default is 0.1
@@ -147,6 +147,7 @@ picrq=function(L,R,delta,x,tau,estimation=NULL,var.estimation=NULL,application=F
         
         if(sum(delta==1)==n){
           ww[i] = 1
+          
         }else if(sum(delta==4)!=0){ 
           sl=approx(c(0,-kml$time,maxit),c(1,1-kml$surv,0), xout=L[i])$y
           sr=approx(c(0,kmr$time,maxit),c(1,kmr$surv,0), xout=R[i])$y
